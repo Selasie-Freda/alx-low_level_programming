9b44cfd10009e907de5c51b42fff89141cde12a6
Selasie-Freda <siesela88@gmail.com>
2023-02-16 08:50:55 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(void)
-	/*
-	 * This function prints the sentence in the puts() function
+	/**
+	 * main - Entry point
+	 *
+	 * Return: Always o (Success)
 	 */
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/*This prints the string in the puts function*/
 	return (0);
 }
