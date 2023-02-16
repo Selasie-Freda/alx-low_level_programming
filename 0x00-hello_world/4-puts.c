d88d5736c1486bff040b5a79e891b9652b821083
Selasie-Freda <siesela88@gmail.com>
2023-02-16 09:13:35 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
+/**
+ * main - entry point 
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/**
-	 * main is required at the beginning
-	 *
-	 * puts is used to print strings.
-	 * It leaves spaces at the end of its outputs
-	 */
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
