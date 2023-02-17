429fa87024351ab2eb85081cae57e4531361b46d
Selasie-Freda <siesela88@gmail.com>
2023-02-17 02:59:37 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
