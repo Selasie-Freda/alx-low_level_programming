994872e2c3bb7e7a3729a9a54802cb451a490aef
Selasie-Freda <siesela88@gmail.com>
2023-02-17 01:40:25 -0800

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
