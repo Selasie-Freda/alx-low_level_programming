5122962ede9266aaaedf863eb2b2bb24f9d3b1ed
Selasie-Freda <siesela88@gmail.com>
2023-02-21 12:26:28 -0800

    write putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,55 +1,6 @@
-#include <unistd.h>
+#ifndef MAIN_H_
+#define MAIN_H_
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/*
-void print_alphabet(void);
+int _putchar(char c);
 
-/**
- * print_alphabet - prints lowercase alphabets to stdout
- *
- * Return: void so 0 on success
- */
-/*
-void print_alphabet(void)
-{
-	int i;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
-}
-
-void print_alphabet_x10(void);
-
-/**
- * print_alphabet_x10 - writes lowercase alphabets a number of times
- *
- * Return: void
- */
-/*
-void print_alphabet_x10(void)
-{
-	int x, repeat;
-
-	repeat = 0;	/*number of times to print to screen*/
-
-/*	while (repeat < 10)
-	{
-		for (x = 'a'; x <= 'z'; x++)
-			_putchar(x);
-			_putchar('\n');
-			repeat++;
-	}
-}*/
+#endif
