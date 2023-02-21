9b269feb29db6fe12432ef72da2bdceadd403395
Selasie-Freda <siesela88@gmail.com>
2023-02-21 14:29:10 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,14 @@
 #include <ctype.h>
 #include "main.h"
 /**
- * Description: return 1 if num is lower
- * returns 0 otherwise
+ * Description - checks if input is lowercase alphabet
+ *
+ * Return: 1 if input is lowercase 0 if not
  */
 int _islower(int c)
 {
 	if (islower(c) != 0)
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
