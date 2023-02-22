52fe3444e450f554672933a8b9897da24a22f005
Selasie-Freda <siesela88@gmail.com>
2023-02-21 20:57:42 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,17 @@
  */
 int print_last_digit(int i)
 {
-	int num;
-	num = i % 10;
-	return (num);
+	int a;
+
+	if (i < 0)
+		i = -i;
+
+	a = i % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
