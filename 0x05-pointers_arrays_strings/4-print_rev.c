3f3346f4da670ff05e0b4cd6dcb26aed72314647
Selasie-Freda <siesela88@gmail.com>
2023-02-27 13:07:43 -0800

    Corrections

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,7 @@ void print_rev(char *s)
 
 	len = strlen(s);
 
-	for (i = len; i >= 0; i--)
+	for (i = len-1; i >= 0; i--)
 	{
 		if (s[i] < 0)
 		{
@@ -21,4 +21,5 @@ void print_rev(char *s)
 		else
 			_putchar(s[i]);
 	}
+	_putchar('\n');
 }
