9a1e49136e7353ac1901f6df11967f8c5dcf85d1
Selasie-Freda <siesela88@gmail.com>
2023-02-27 17:21:04 -0800

    Strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - acts in similar form as strcpy
+ * @dest: argument
+ * @src; argument
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *tmp = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (tmp);
+}
