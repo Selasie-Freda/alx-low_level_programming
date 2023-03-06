cd77ce3a1e7dd646feebab5b84d97fddf18e17c6
Selasie-Freda <siesela88@gmail.com>
2023-03-06 08:52:41 -0800

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard - prints chessboard
+ * @a: the board to print
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, x;
+
+	i = 0;
+	while (i < 8)
+	{
+		x = 0;
+		while (x < 8)
+		{
+			_putchar(a[i][x]);
+			x++;
+		}
+		i++;
+	}
+}
