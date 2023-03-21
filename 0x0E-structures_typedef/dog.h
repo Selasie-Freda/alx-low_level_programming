7c0dcc89e3f94f8119a2386099de02b4d7b87675
Selasie-Freda <siesela88@gmail.com>
2023-03-21 10:47:35 -0700

    struct definition

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct dog - used to define a new structure
+ * @name: element 1
+ * @age: element 2
+ * @owner: element 3
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
