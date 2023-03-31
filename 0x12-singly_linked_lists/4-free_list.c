e5b7c6a09b52de68884ac675a573206783a21136
Selasie-Freda <siesela88@gmail.com>
2023-03-31 12:45:28 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list_t
+ * @head: pointer
+ */
+
+void free_list(list_t *head)
+{
+	list_t *nodes;
+
+	nodes = NULL;
+	
+	while (head)
+	{
+		free(head->str);
+		nodes = head;
+		head = head->next;
+		free(nodes);
+	}
+}
