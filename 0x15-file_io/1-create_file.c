b004e1ca588c738679a43a406c207bacd499b2d7
Selasie-Freda <siesela88@gmail.com>
2023-04-10 15:35:29 -0700

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,26 +8,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t len = 0;
+	int fd;
+	ssize_t len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return (-1);
-
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            len++;
-
-        if (write(fd, text_content, len) == -1)
-            return (-1);
-    }
-
-    close(fd);
-
-    return (1);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+		if (write(fd, text_content, len) == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
 }
